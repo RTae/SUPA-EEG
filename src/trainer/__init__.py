81ef64c36afc99392073023a1cdd8cda1f62d3ca
rtae <rtae@gmail.com>
2026-05-30 10:03:54 +0000

    Add loss functions to __all__ export in trainer module

--- src/trainer/__init__.py
+++ src/trainer/__init__.py
@@ -1,6 +1,16 @@
+from .loss import (
+    compute_loss,
+    info_nce_loss,
+    l1_sparsity_loss,
+    sigreg_loss,
+)
 from .metrics import retrieve_all, retrieve_topk
 
 __all__ = [
+    "compute_loss",
+    "info_nce_loss",
+    "l1_sparsity_loss",
+    "sigreg_loss",
     "retrieve_all",
     "retrieve_topk",
 ]
